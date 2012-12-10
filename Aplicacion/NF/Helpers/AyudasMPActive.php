e36178aa9105e390346c12f37c092d4f4e5270c2
Neural Framework <neural.framework@gmail.com>
2012-12-10 04:04:18 -0500

    Add Actualizacion de Archivo de Mi Aplicacion
    Actualizacion de Archivo de Mi Aplicacion

--- Aplicacion/NF/Helpers/AyudasMPActive.php
+++ Aplicacion/NF/Helpers/AyudasMPActive.php
@@ -1,5 +1,18 @@
 <?php
     
+    # #####################################################################################
+	#
+	#	Neural Framework (Framework en PHP)
+	#
+	#	@author: 	Zyos Ilusionista <neural.framework@gmail.com> <Zyos_Ilusionista@hotmail.com>
+	#	@license: 	Licencia GNU/GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
+	#
+	#	Edicion Beta de Lanzamiento de Neural Framework.
+	#
+	#	http://github.com/neuralframework/
+	#
+	# #####################################################################################
+	
     abstract class AyudasMPActive {
         
         public static function Active($Controlador) {
